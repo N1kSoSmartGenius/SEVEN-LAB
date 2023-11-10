2605b4e9281f1757a958ff724ddf22bf4204c0a1
N1kSoSmartGenius <nik.garbuzov05@mail.ru>
2023-11-10 19:31:49 +0300

    SEVEN LAB

---
+++ sevenlab.py
@@ -0,0 +1,24 @@
+import telebot
+from telebot import types
+
+token = "5852987734:AAEPHHl-A6FBJXypx9gvZ8e6RvIiGSr2E80"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Хочу","/help")
+    bot.send_message(message.chat.id, 'Здравствуйте! Хотите узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я умею выводить т')
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text.lower() == "хочу":
+        bot.send_message(message.chat.id, 'Тогда вам сюда - https://mtuci.ru/')
+
+
+bot.polling()
